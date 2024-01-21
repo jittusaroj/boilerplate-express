35711712bab55fe3ab6ed59c04ed22fc7cc1ec9a
jitendra kumar saroj <saroj222137@gmail.com>
2024-01-21 14:52:44 +0530

    added mongoose

--- myApp.js
+++ myApp.js
@@ -3,6 +3,10 @@ let express = require('express');
 const bodyParser = require("body-parser");
 let app = express();
 const path = require('path');
+const mongoose = require("mongoose")
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(function middleware(req, res, next) {
