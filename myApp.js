9eea61e241e861e5bcc180412eb73acb366e4d45
jitendra kumar saroj <saroj222137@gmail.com>
2024-01-21 11:32:16 +0530

    added index.html

--- myApp.js
+++ myApp.js
@@ -1,11 +1,12 @@
 let express = require('express');
 let app = express();
 
+
 console.log("Hello World");
 
 app.get("/",(req,res)=>{
 console.log("Hello Express");
-res.send("Hello Express");
+res.sendFile(__dirname+"/views/index.html");
 })
 
 
