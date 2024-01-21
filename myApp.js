ea7e51814dc21d2d92f2af72347e769c31c10b64
jitendra kumar saroj <saroj222137@gmail.com>
2024-01-21 14:27:52 +0530

    env message

--- myApp.js
+++ myApp.js
@@ -1,8 +1,9 @@
+require('dotenv').config()
 let express = require('express');
 const bodyParser = require("body-parser");
 let app = express();
 const path = require('path');
-require('dotenv').config()
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(function middleware(req, res, next) {
     var string = req.method + " " + req.path + " - " + req.ip;
@@ -18,15 +19,13 @@ app.use('/public', express.static(path.join(__dirname, 'public')));
 app.get("/",(req,res)=>{
 res.sendFile(__dirname+"/views/index.html");
 })
-app.get("/json",(req,res)=>{
-    let response="";
-    if (process.env.MESSAGE_STYLE === "uppercase") {
-        response = "Hello World".toUpperCase();
-      } else {
-        response = "Hello World";
-      }
-      res.json({response})
-})
+app.get('/json', (req, res) => {
+    let responseMessage = 'Hello json';
+    if (process.env.MESSAGE_STYLE === 'uppercase') {
+      responseMessage = responseMessage.toUpperCase();
+    }
+      res.json({ message: responseMessage });
+  });
 app.get("/now",(req,res,next)=>{
     const now=new Date().toString();
     req.time=now;
