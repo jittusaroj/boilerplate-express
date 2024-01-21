939b6101715e4b8ce4b03494313efeaa47a10992
jitendra kumar saroj <saroj222137@gmail.com>
2024-01-21 11:38:56 +0530

    make public folder public

--- myApp.js
+++ myApp.js
@@ -1,11 +1,12 @@
 let express = require('express');
 let app = express();
 
-
+app.use(express.static(__dirname + "/public"));
 console.log("Hello World");
 
 app.get("/",(req,res)=>{
 console.log("Hello Express");
+console.log(__dirname);
 res.sendFile(__dirname+"/views/index.html");
 })
 
