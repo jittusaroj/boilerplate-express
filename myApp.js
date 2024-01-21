98984e40aecf8aee63729e4bdefc1cd6c014e0b7
jitendra kumar saroj <saroj222137@gmail.com>
2024-01-21 11:47:20 +0530

    added path and use

--- myApp.js
+++ myApp.js
@@ -1,8 +1,10 @@
 let express = require('express');
 let app = express();
+const path = require('path');
 
-app.use(express.static(__dirname + "/public"));
+app.use("/public",express.static(`${__dirname}/public`));
 console.log("Hello World");
+app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.get("/",(req,res)=>{
 console.log("Hello Express");
